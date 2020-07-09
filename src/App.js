5db0dff6e84783e0175bf1d927f439e64bb86729
AbdulRahimFarooqui <abdurraheemfarooqui@gmail.com>
2020-07-09 11:54:52 +0500

    Did nothing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import MediaCard from './MediaCard';
 function App() {
   return (
     <div><MediaCard title = {<h1>The Title</h1>} body = {<b>This is the body text</b>} imageURL = 'https://daveceddia.com/images/social-card.png'/>
-    
     </div>
   );
 }
